6b203b9d281e366f3e4bee0ede48c59d720b3b2b
Magnus Henoch <magnus.henoch@gmail.com>
2015-07-30 17:09:59 +0100

    Basic files for leviathan_rest
    It starts up, and asks erl_cowboy to route an empty set of paths.

---
+++ src/leviathan_rest_app.erl
@@ -0,0 +1,22 @@
+-module(leviathan_rest_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    ok = start_cowboy(),
+    leviathan_rest_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+start_cowboy() ->
+    ok = erl_cowboy:routing(?MODULE,
+        [
+        ]).
