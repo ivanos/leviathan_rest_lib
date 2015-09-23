f07e19f0abb23b76ea677571c033937a4b1e29e0
Magnus Henoch <magnus.henoch@gmail.com>
2015-09-23 12:41:25 +0100

    Add handler for starting OpenFlow switches

---
+++ src/lr_switch_handler.erl
@@ -0,0 +1,24 @@
+-module(lr_switch_handler).
+
+-behaviour(cowboy_http_handler).
+
+-export([init/3, handle/2, terminate/3]).
+
+init(_, Req, _Opts) ->
+    {ok, Req, no_state}.
+
+handle(Req, State) ->
+    {Method, Req2} = cowboy_req:method(Req),
+    handle(Method, Req2, State).
+
+handle(<<"POST">>, Req, State) ->
+    {ok, JsonBin, Req2} = cowboy_req:body(Req),
+    leviathan_switch:import_binary(JsonBin),
+    {ok, Req2, State};
+handle(_, Req, State) ->
+    {ok, Req2} = cowboy_req:reply(405, [{<<"content-type">>, <<"text/plain">>}],
+				  <<"Method not allowed\n">>, Req),
+    {ok, Req2, State}.
+
+terminate(_Reason, _Req, _State) ->
+    ok.
