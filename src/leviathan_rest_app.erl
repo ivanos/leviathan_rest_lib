9a169af93960b473bebcd2a4f633950edb0400bf
Szymon Mentel <szymon.mentel@erlang-solutions.com>
2015-11-12 12:31:32 +0100

    Refactor routing setup

--- src/leviathan_rest_app.erl
+++ src/leviathan_rest_app.erl
@@ -17,22 +17,52 @@ start(_StartType, _StartArgs) ->
 stop(_State) ->
     ok.
 
+%% ===================================================================
+%% Internal functions
+%% ===================================================================
+
 start_cowboy() ->
     ok = erl_cowboy:routing(?MODULE,
-        [
-         {"/cen/import", lr_cen_handler, #{handle_action => import}},
-         {"/cen/make", lr_cen_handler, #{handle_action => make}},
-         {"/cen/destroy", lr_cen_handler, #{handle_action => destroy}},
-         {"/host/:host/:container/:cen", lr_container_cen_handler, []},
-         {"/host/:host/:container", lr_container_handler, []},
-         {"/cen/:cen", lr_cenid_handler, []},
-         {"/cin/import", lr_cin_handler, #{handle_action => import}},
-         {"/cin/make", lr_cin_handler, #{handle_action => make}},
-         {"/cin/destroy", lr_cin_handler, #{handle_action => destroy}},
-         {"/cpool/", lr_cpool_handler, []},
-         {"/switch/", lr_switch_handler, []},
-         {"/leviathan/monitor", lr_monitor_handler, []},
-         {"/leviathan/monitor/test/[...]", cowboy_static, {priv_dir, leviathan_rest, "static"}}
-        ]).
+                            lists:flatten([
+                                           cen_routes(),
+                                           cin_routes(),
+                                           container_routes(),
+                                           monitor_routes(),
+                                           cpool_routes(),
+                                           switch_routes()
+                                          ])).
+
+cen_routes() ->
+    [
+     {"/cen/import", lr_cen_handler, #{handle_action => import}},
+     {"/cen/make", lr_cen_handler, #{handle_action => make}},
+     {"/cen/destroy", lr_cen_handler, #{handle_action => destroy}},
+     {"/cen/:cen", lr_cenid_handler, []}
+    ].
+
+cin_routes() ->
+    [
+     {"/cin/import", lr_cin_handler, #{handle_action => import}},
+     {"/cin/make", lr_cin_handler, #{handle_action => make}},
+     {"/cin/destroy", lr_cin_handler, #{handle_action => destroy}}
+    ].
+
+container_routes() ->
+    [
+     {"/host/:host/:container/:cen", lr_container_cen_handler, []},
+     {"/host/:host/:container", lr_container_handler, []}
+    ].
+
+monitor_routes() ->
+    [
+     {"/leviathan/monitor", lr_monitor_handler, []},
+     {"/leviathan/monitor/test/[...]", cowboy_static, {priv_dir, leviathan_rest, "static"}}
+    ].
+
+cpool_routes() ->
+    [{"/cpool/", lr_cpool_handler, []}].
+
+switch_routes() ->
+    [{"/switch/", lr_switch_handler, []}].
 
 
